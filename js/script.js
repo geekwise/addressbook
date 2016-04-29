845f951c088e53bb9aa1a01c99da756b4c3a1e1c
Kao Thao <kausthoj@gmail.com>
2016-04-29 19:20:58 +0000

    started second display screen for when plus button is clicked and added right column for blue letters

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 var contact_container;
 var top_row_container;
+var search_container;
+var names_container;
+var right_column_letters_container;
+var search;
 var alphabet;
 var split_alphabet;
-var search_container;
-var search;
+
+var new_contact_container;
+var second_top_row_container;
+
+//building a container for the All Contacts display screen
 
 contact_container = document.createElement('div');
 contact_container.setAttribute('id','contact_container');
@@ -18,6 +25,12 @@ search = document.createElement('input');
 search.setAttribute('id','search');
 search.setAttribute('placeholder','Search');
 
+names_container = document.createElement('div');
+names_container.setAttribute('id','names_container');
+
+right_column_letters_container = document.createElement('div');
+right_column_letters_container.setAttribute('id','right_column_letters_container');
+
 var create_element = function(element_type,element_id){
     var element = document.createElement(element_type);
     element.setAttribute('id',element_id);
@@ -27,7 +40,13 @@ var create_element = function(element_type,element_id){
 var create_contact_container_elements = function(contact_container_element, contact_container_id){
     var contact_container_element = document.createElement(contact_container_element);
     contact_container_element.setAttribute('id',contact_container_id);
-    contact_container.appendChild(contact_container_element);
+    names_container.appendChild(contact_container_element);
+};
+
+var create_right_column_letters = function(letter_element,letter_element_id){
+    var right_column_letters = document.createElement(letter_element);
+    right_column_letters.setAttribute('id',letter_element_id);
+    right_column_letters_container.appendChild(right_column_letters);
 };
 
 var contact_information = function(first_name,last_name,company,phone,email){
@@ -38,11 +57,33 @@ var contact_information = function(first_name,last_name,company,phone,email){
     this.email = email;
 };
 
+//building a container for the New Contact display screen
+
+new_contact_container = document.createElement('div');
+new_contact_container.setAttribute('id','new_contact_container');
+
+second_top_row_container = document.createElement('div');
+second_top_row_container.setAttribute('id','second_top_row_container');
+
+var create_second_top_row_element = function(element_type,element_id){
+    var element = document.createElement(element_type);
+    element.setAttribute('id',element_id);
+    second_top_row_container.appendChild(element);
+};
+
+
+
+//When DOM is loaded
+
 document.addEventListener('DOMContentLoaded',function(event){
     
+// Creating All Contacts disply screen
+
     document.body.appendChild(contact_container);
     contact_container.appendChild(top_row_container);
     contact_container.appendChild(search_container);
+    contact_container.appendChild(names_container);
+    contact_container.appendChild(right_column_letters_container);
     search_container.appendChild(search);
     
     create_element('span','group_button');
@@ -66,7 +107,6 @@ document.addEventListener('DOMContentLoaded',function(event){
     var full_name_container;
     
     for(var i=0; i<26; i++){
-        
         create_contact_container_elements('div','abc_container_' + i);
         abc_container = document.getElementById('abc_container_' + i);
         letter_container = document.createElement('p');
@@ -74,6 +114,12 @@ document.addEventListener('DOMContentLoaded',function(event){
         letter_container.textContent = split_alphabet[i];
         
         abc_container.appendChild(letter_container); 
+    };
+    
+    for(var i=0; i<26; i++){
+        create_right_column_letters('p','right_column_letters_'+i);
+        var letters = document.getElementById('right_column_letters_'+i);
+        letters.textContent = split_alphabet[i];
     };
     
     var alonzo = new contact_information('Alonzo','Yrigollen','EDC','(559) 111-1111','geekwise.alonzo.yrigollen@gmail.com');
@@ -114,6 +160,23 @@ document.addEventListener('DOMContentLoaded',function(event){
         console.log(current_full_name_container.textContent[0]);
             
     };
+    
+// Creating New Contact display screen
+
+    document.body.appendChild(new_contact_container);
+    new_contact_container.appendChild(second_top_row_container);
+    
+    create_second_top_row_element('span','cancel_button');
+    create_second_top_row_element('span','new_contact');
+    create_second_top_row_element('span','done_button');
+    
+    var cancel = document.getElementById('cancel_button');
+    var new_contact = document.getElementById('new_contact');
+    var done_button = document.getElementById('done_button');
+    
+    cancel.textContent = 'Cancel';
+    new_contact.textContent = 'New Contact';
+    done_button.textContent = 'Done';
     
 });
 
