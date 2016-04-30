49452c9175db084161d3e358bf0f1d70f96535ca
Kao Thao <kausthoj@gmail.com>
2016-04-30 18:37:01 +0000

    able to pre populate default contacts to All Contact display screen

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@ var second_top_row_container;
 var left_input_section_container_div;
 var right_input_section_container_div;
 
+var abc_container;
+var letter_container;
+var full_name_container;
+
 //building a container for the All Contacts display screen
 
 contact_container = document.createElement('div');
@@ -64,6 +68,27 @@ var contact_information = function(first_name,last_name,company,phone,email){
     this.phone = phone;
     this.email = email;
 };
+
+// create the default contact information
+
+var alonzo = new contact_information('Alonzo','Yrigollen','EDC','(559) 111-1111','geekwise.alonzo.yrigollen@gmail.com');
+var j = new contact_information('J','Tablett','EDC','(559) 111-1112','geekwise.jennifer.tablett@gmail.com');
+var tiffany = new contact_information('Tiffany','Ranish','EDC','(559) 111-1113','geekwise.tiffany.ranish@gmail.com');
+var nicole = new contact_information('Nicole','Deltoro','EDC','(559) 111-1114','geekwise.nicole.deltoro@gmail.com');
+var veronica = new contact_information('Veronica','Chavez','EDC','(559) 111-1115','geekwise.veronica.chavez@gmail.com');
+var juston = new contact_information('Juston','Miller','EDC','(559) 111-1116','geekwise.juston.miller@gmail.com');
+
+// create an array for all the default contacts
+
+var contact_array = [
+    alonzo,
+    j,
+    tiffany,
+    nicole,
+    veronica,
+    juston
+];
+
 
 //building a container for the New Contact display screen
 
@@ -130,10 +155,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     //console.log(alphabet);
     split_alphabet = alphabet.split('');
     
-    var abc_container;
-    var letter_container;
-    var full_name_container;
-    
+// create the left side letters and containers for the contacts 
     for(var i=0; i<26; i++){
         create_contact_container_elements('div','abc_container_' + i);
         abc_container = document.getElementById('abc_container_' + i);
@@ -152,43 +174,47 @@ document.addEventListener('DOMContentLoaded',function(event){
         right_column_letters_container.appendChild(hashtag);
     };
     
-    var alonzo = new contact_information('Alonzo','Yrigollen','EDC','(559) 111-1111','geekwise.alonzo.yrigollen@gmail.com');
-    var j = new contact_information('J','Tablett','EDC','(559) 111-1112','geekwise.jennifer.tablett@gmail.com');
-    var tiffany = new contact_information('Tiffany','Ranish','EDC','(559) 111-1113','geekwise.tiffany.ranish@gmail.com');
-    var nicole = new contact_information('Nicole','Deltoro','EDC','(559) 111-1114','geekwise.nicole.deltoro@gmail.com');
-    var veronica = new contact_information('Veronica','Chavez','EDC','(559) 111-1115','geekwise.veronica.chavez@gmail.com');
-    var juston = new contact_information('Juston','Miller','EDC','(559) 111-1116','geekwise.juston.miller@gmail.com');
-    
-    var contact_array = [
-        alonzo,
-        j,
-        tiffany,
-        nicole,
-        veronica,
-        juston
-    ];
-    
-    for(i in contact_array){
-        full_name_container = document.createElement('p');
-        full_name_container.setAttribute('id','full_name_container_'+i);
-        full_name_container.textContent = contact_array[i].first_name + ' ' + contact_array[i].last_name;
+    // for(i in contact_array){
+    //     full_name_container = document.createElement('p');
+    //     full_name_container.setAttribute('id','full_name_container_'+i);
+    //     full_name_container.textContent = contact_array[i].first_name + ' ' + contact_array[i].last_name;
         
-        abc_container.appendChild(full_name_container);
+    //     abc_container.appendChild(full_name_container);
         
-        var current_full_name_container = document.getElementById('full_name_container_' + i);
-        var current_letter_container = document.getElementById('letter_container_' + i);
-        var current_abc_container = document.getElementById('abc_container_' + i);
+    //     var current_full_name_container = document.getElementById('full_name_container_' + i);
+    //     var current_letter_container = document.getElementById('letter_container_' + i);
+    //     var current_abc_container = document.getElementById('abc_container_' + i);
                     
-        if(current_full_name_container.textContent[0] === current_letter_container.textContent[0]){
+    //     if(current_full_name_container.textContent[0] === current_letter_container.textContent[0]){
                             
-            current_abc_container.appendChild(current_full_name_container);
-            //still need to add code to append all current_full_name_container to specific current_abc_container
-            //right now, only Alonzo's full name is working
-        }
+    //         current_abc_container.appendChild(current_full_name_container);
+    //         //still need to add code to append all current_full_name_container to specific current_abc_container
+    //         //right now, only Alonzo's full name is working
+    //     }
             
-        console.log(current_full_name_container);
-        console.log(current_full_name_container.textContent[0]);
+    //     console.log(current_full_name_container);
+    //     console.log(current_full_name_container.textContent[0]);
             
+    // };
+    
+// create array for all the letter_container id's
+
+    for(i in split_alphabet){
+        var current_letter = split_alphabet[i];
+        var current_abc_container = document.getElementById('abc_container_'+i);
+        
+        for(i in contact_array){
+            current_letter = current_letter.toLowerCase();
+            var regex_pattern = new RegExp('^' + current_letter + '.*|\w','gi')
+            
+            if(contact_array[i].first_name.match(regex_pattern)){
+                var inner_contact_container = document.createElement('p');
+                inner_contact_container.setAttribute('class','inner_contact_container');
+                inner_contact_container.textContent = contact_array[i].first_name + ' ' + contact_array[i].last_name;
+                
+                current_abc_container.appendChild(inner_contact_container);
+            };
+        };
     };
     
 // Creating New Contact display screen
