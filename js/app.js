0081d9a2bf04ca29515d06a101933ee50bf05489
GeekWise <cnt+geekwise@cntmedia.com>
2016-05-03 22:46:42 +0000

    dynamic loading of javascript files

---
+++ js/app.js
@@ -0,0 +1,54 @@
+var data_urls = window.location.origin+'/js/data_urls.js';
+var load_js_library = 'https://cdnjs.cloudflare.com/ajax/libs/load.js/1316434407/load-min.js';
+
+var load_scripts = function(){
+    
+    load(data_urls).thenRun(function(){
+       
+       load(contact_first_name_url,contact_last_name_url).thenRun(function(){
+           
+                  console.log(first_name); 
+                   console.log(last_name);
+
+       })
+       
+        
+    })
+    
+    
+}
+
+
+var attach_script = function(url, callback) {
+	
+	var script_element = document.createElement('script');
+	var first_script = document.getElementsByTagName('script')[0];
+	
+	//script_element.src = '//' + url;
+	script_element.src = url;
+	
+  	if(callback){
+		script_element.addEventListener('load',function(event){
+				callback(null, event);
+		},false);
+	}
+	
+	first_script.parentNode.insertBefore(script_element,first_script);
+
+}
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+
+            
+
+            attach_script(load_js_library,function(){
+                    
+                    console.log('load.js ready');
+                    load_scripts();
+
+            });        
+
+    
+    
+});
---
+++ js/data_urls.js
@@ -0,0 +1,3 @@
+var contact_first_name_url = 'https://rawgit.com/geekwise/a948983b15c6891cce98040a3bca84c5/raw/81efd0551a5621628e1fe84bc09f0603c6a31afb/first_name.js';
+var contact_last_name_url = 'https://rawgit.com/geekwise/620f5c9320a855180381a72ec2df9b68/raw/c213fc58caf25406e126be69811769b6c9bd5d91/last_name.js';
+var load_js_library = 'https://cdnjs.cloudflare.com/ajax/libs/load.js/1316434407/load-min.js';
