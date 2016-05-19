83c0c0919a532f44ef1ba717dd2e60d5754e5757
Kao Thao <kausthoj@gmail.com>
2016-05-19 21:36:23 +0000

    added loop to create containers for infinite phone entry

--- js/script.js
+++ js/script.js
@@ -348,6 +348,13 @@ document.addEventListener('DOMContentLoaded',function(event){
     create_containers('div','add_other_info_container',get_element('new_contact_container'));
     create_multi_elements(15,'div','add_more_info_container_',get_element('add_other_info_container'));
     
+    var append_element_before = function(){
+        for(var i=0; i<1000; i++){
+            get_element('add_more_info_container_'+i).parentNode.insertAfter(get_element('add_more_info_container_'+i),get_element('infinite_container_'+i))
+        };
+        
+    };
+    
     var create_plus_containers = function(){
         for(var i=0; i<get_element('add_other_info_container').childElementCount; i++){
             create_containers('div','plus_and_add_containers_'+i,get_element('add_more_info_container_'+i));
@@ -357,6 +364,7 @@ document.addEventListener('DOMContentLoaded',function(event){
             get_element('green_plus_container_'+i).setAttribute('class','green_plus fa fa-plus-circle');
             
         };
+        
     };
     
 //create containers after clicking on add phone
@@ -416,21 +424,32 @@ document.addEventListener('DOMContentLoaded',function(event){
     var add_phone_button = get_element('plus_and_add_containers_0');
     add_phone_button.addEventListener("click", function (event) {
     click++;
-    //for(var i=0; i<get_element(''))
-    if (click == 1) {
-    		get_element('main_label_container_0').style.display = 'inline-block';
-    }
-    else if (click == 2) {
-    		get_element('main_label_container_1').style.display = 'inline-block';
-    }
-    else if (click == 3) {
-    		get_element('main_label_container_2').style.display = 'inline-block';
-    }
-    else{
-    		
-    }
+    for(var i=0; i<100; i++){
+        if(click == i+1){
+    		get_element('main_label_container_'+i).style.display = 'inline-block';
+        }else if(click == i+10) {
+            
+            create_containers('div','infinite_container_'+i,get_element('add_more_info_container_0'));
+            
+    		create_containers('div','infinite_left_container_'+i,get_element('infinite_container_'+i));
+            create_containers('div','infinite_right_container_'+i,get_element('infinite_container_'+i));
+            
+            create_containers('input','infinte_phone_input_'+i,get_element('infinite_right_container_'+i));
+            
+            create_containers('div','infinite_minus_container_'+i,get_element('infinite_left_container_'+i));
+            create_containers('div','infinte_label_container_'+i,get_element('infinite_left_container_'+i));
+            create_containers('div','infinte_chevron_container_'+i,get_element('infinite_left_container_'+i));
+            
+            get_element('infinite_minus_container_'+i).setAttribute('class','minus_container fa fa-minus-circle');
+            get_element('infinte_chevron_container_'+i).setAttribute('class','fa fa-chevron-right');
+            get_element('infinte_phone_input_'+i).setAttribute('placeholder','Phone');
+            get_element('infinite_phone_input_'+i).setAttribute('class','first_last_company phone_input');
+            get_element('infinte_label_container_'+i).textContent = 'home';
     
-    //elem.removeEventListener("click", h);
+        };
+    };
+    
+    
 });
         
     
