0942227b507882cc0718fa7ad633f10fbbf674f8
x <x@x.com>
2016-06-02 17:03:15 -0700

    added in controller.js from @kao 06-02-2016

---
+++ js/controller.js
@@ -0,0 +1,7 @@
+var contact_information = function(first_name,last_name,company,phone,email){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.company = company;
+    this.phone = phone;
+    this.email = email;
+};
