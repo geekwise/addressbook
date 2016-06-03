716a26dbf6b8a9b33fc91ba3c1cfb337660086ca
x <x@x.com>
2016-06-02 17:04:28 -0700

    added in model.js from @kao 06-02-2016

---
+++ js/model.js
@@ -0,0 +1,58 @@
+var alonzo = new contact_information('Alonzo','Yrigollen','EDC','(559) 111-1111','geekwise.alonzo.yrigollen@gmail.com');
+var j = new contact_information('J','Tablett','EDC','(559) 111-1112','geekwise.jennifer.tablett@gmail.com');
+var tiffany = new contact_information('Tiffany','Ranish','EDC','(559) 111-1113','geekwise.tiffany.ranish@gmail.com');
+var nicole = new contact_information('Nicole','Deltoro','EDC','(559) 111-1114','geekwise.nicole.deltoro@gmail.com');
+var veronica = new contact_information('Veronica','Chavez','EDC','(559) 111-1115','geekwise.veronica.chavez@gmail.com');
+var juston = new contact_information('Juston','Miller','EDC','(559) 111-1116','geekwise.juston.miller@gmail.com'); 
+var mark = new contact_information('Mark','Thomas','EDC','(559) 111-1116','mark.thomas.miller@gmail.com'); 
+var bob = new contact_information('Bob','Brettson','EDC','(559) 111-1116','mark.thomas.miller@gmail.com'); 
+var sarah = new contact_information('Sarah','Smith','EDC','(559) 111-1116','mark.thomas.miller@gmail.com'); 
+
+var contact_array = [
+    alonzo,
+    juston,
+    tiffany,
+    nicole,
+    veronica,
+    j,
+    mark,
+    bob,
+    sarah
+];
+
+var add_info_array = [
+    'add phone',
+    'add email',
+    'Ringtone  Default',
+    'Vibration  Default',
+    'Text Tone  Default',
+    'Vibration  Default',
+    'add url',
+    'add address',
+    'add birthday',
+    'add date',
+    'add related name',
+    'add social profile',
+    'add instant message',
+    '',
+    'add field'
+];
+
+// array of content for containers after clicking on add phone
+
+    var label_array = [
+        'home',
+        'work',
+        'iphone',
+        'mobile',
+        'main',
+        'home fax',
+        'work fax',
+        'pager',
+        'other'
+    ];
+
+
+
+
+
